bd3244f41900a1923c2166afbbb37786ec6cf324
lateefa <aldoseri369@gmail.com>
2025-02-23 12:07:10 +0300

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,70 +9,70 @@ const zombieFighters = [
     price: 12,
     strength: 6,
     agility: 4,
-    img: 'https://via.placeholder.com/150/92c952',
+    img: 'https://loremflickr.com/320/240?random=1',
   },
   {
     name: 'Scavenger',
     price: 10,
     strength: 5,
     agility: 5,
-    img: 'https://via.placeholder.com/150/771796',
+    img: 'https://loremflickr.com/320/240?random=2',
   },
   {
     name: 'Shadow',
     price: 18,
     strength: 7,
     agility: 8,
-    img: 'https://via.placeholder.com/150/24f355',
+    img: 'https://loremflickr.com/320/240?random=3',
   },
   {
     name: 'Tracker',
     price: 14,
     strength: 7,
     agility: 6,
-    img: 'https://via.placeholder.com/150/d32776',
+    img: 'https://loremflickr.com/320/240?random=4',
   },
   {
     name: 'Sharpshooter',
     price: 20,
     strength: 6,
     agility: 8,
-    img: 'https://via.placeholder.com/150/1ee8a4',
+    img: 'https://loremflickr.com/320/240?random=5',
   },
   {
     name: 'Medic',
     price: 15,
     strength: 5,
     agility: 7,
-    img: 'https://via.placeholder.com/150/66b7d2',
+    img: 'https://loremflickr.com/320/240?random=6',
   },
   {
     name: 'Engineer',
     price: 16,
     strength: 6,
     agility: 5,
-    img: 'https://via.placeholder.com/150/56acb2',
+    img: 'https://loremflickr.com/320/240?random=7',
   },
   {
     name: 'Brawler',
     price: 11,
     strength: 8,
     agility: 3,
-    img: 'https://via.placeholder.com/150/8985dc',
+    img: 'https://loremflickr.com/320/240?random=8',
   },
   {
     name: 'Infiltrator',
     price: 17,
     strength: 5,
     agility: 9,
-    img: 'https://via.placeholder.com/150/392537',
+    img: 'https://loremflickr.com/320/240?random=9',
   },
   {
     name: 'Leader',
     price: 22,
     strength: 7,
     agility: 6,
-    img: 'https://via.placeholder.com/150/602b9e',
+    img: 'https://loremflickr.com/320/240?random=10',
   },
 ];
 
@@ -106,7 +106,7 @@ const App = () => {
    
   
   return (
-    <>
+    <div>
     <h1>zombie Fighters</h1>
     <h4>Money : {money}</h4>
     <h4>Team Strength :{strength} </h4>
@@ -137,7 +137,7 @@ const App = () => {
 
     </section>
       
-  </>
+  </div>
 );
   
 }
